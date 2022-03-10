1f9e67948f1334ab3918ce49c22a2cdd015d928f
MargaridaD <97111622+MargaridaD@users.noreply.github.com>
2022-03-10 18:00:17 +0000

    Aterei AppStore
    Criei função carregar

--- src/projetoExtra1/AppStore.java
+++ src/projetoExtra1/AppStore.java
@@ -23,8 +23,56 @@ public class AppStore {
 		aplicacoes = new ArrayList<Aplicacao>();
 		clientes = new ArrayList<Cliente>();
 		compras = new ArrayList<Compra>();
+		carregar();
+		
 	}
 	
+	private void carregar() {
+		//Instanciar Programadores e acrescentar à lista da AppStore
+		Programador programador1 = new Programador("A",20);
+		Programador programador2 = new Programador("B",30);
+		Programador programador3 = new Programador("C",40);
+		adicionarProgramador(programador1);
+		adicionarProgramador(programador2);
+		adicionarProgramador(programador3);
+
+		
+		//Instanciar Aplicações e acrescentar às listas da AppStore e do programador
+		Aplicacao aplicacao1 = new Aplicacao("A1", 5, programador1, TipoAplicacao.GAMES);
+		Aplicacao aplicacao2 = new Aplicacao("A2", 10, programador1, TipoAplicacao.BUSINESS);
+		Aplicacao aplicacao3 = new Aplicacao("A3", 15, programador1, TipoAplicacao.GAMES);
+		Aplicacao aplicacao4 = new Aplicacao("A4", 20, programador1, TipoAplicacao.EDUCATION);
+		Aplicacao aplicacao5 = new Aplicacao("B1", 7, programador2, TipoAplicacao.GAMES);
+		Aplicacao aplicacao6 = new Aplicacao("B2", 17, programador2, TipoAplicacao.EDUCATION);
+		Aplicacao aplicacao7 = new Aplicacao("B3", 27, programador2, TipoAplicacao.GAMES);
+		Aplicacao aplicacao8 = new Aplicacao("C1", 4, programador3, TipoAplicacao.GAMES);
+		Aplicacao aplicacao9 = new Aplicacao("C2", 8, programador3, TipoAplicacao.GAMES);
+		Aplicacao aplicacao10 = new Aplicacao("C3", 12, programador3, TipoAplicacao.TRAVEL);
+		Aplicacao aplicacao11 = new Aplicacao("C4", 16, programador3, TipoAplicacao.TRAVEL);
+		Aplicacao aplicacao12 = new Aplicacao("C5", 20, programador3, TipoAplicacao.EDUCATION);
+
+		adicionarAplicacao(aplicacao1);
+		adicionarAplicacao(aplicacao2);
+		adicionarAplicacao(aplicacao3);
+		adicionarAplicacao(aplicacao4);
+		adicionarAplicacao(aplicacao5);
+		adicionarAplicacao(aplicacao6);
+		adicionarAplicacao(aplicacao7);
+		adicionarAplicacao(aplicacao8);
+		adicionarAplicacao(aplicacao9);
+		adicionarAplicacao(aplicacao10);
+		adicionarAplicacao(aplicacao11);
+		adicionarAplicacao(aplicacao12);
+		
+		//Instanciar Clientes e acrescentar à lista da AppStore
+		Cliente cliente1 = new Cliente("D",11);
+		Cliente cliente2 = new Cliente("E",21);
+		Cliente cliente3 = new Cliente("F",31);
+		
+		adicionarCliente(cliente1);
+		adicionarCliente(cliente2);
+		adicionarCliente(cliente3);
+	}
 	//Metodos
 	public void adicionarProgramador(Programador aProgramador) {
 		programadores.add(aProgramador);
@@ -84,8 +132,31 @@ public class AppStore {
 		}
 		
 	}
+		//Sair da aplicação.
+
+	public ArrayList<Programador> getProgramadores() {
+		return programadores;
+	}
+
+
+
+	public ArrayList<Aplicacao> getAplicacoes() {
+		return aplicacoes;
+	}
+
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+
+
+	public ArrayList<Compra> getCompras() {
+		return compras;
+	}
+
+
 	
-	//Sair da aplicação.
 
 	
 }
--- src/projetoExtra1/Main.java
+++ src/projetoExtra1/Main.java
@@ -12,50 +12,7 @@ public class Main {
 	//Instanciar AppStore
 	AppStore appStore1 = new AppStore();
 		
-	//Instanciar Programadores e acrescentar à lista da AppStore
-	Programador programador1 = new Programador("A",20);
-	Programador programador2 = new Programador("B",30);
-	Programador programador3 = new Programador("C",40);
-	appStore1.adicionarProgramador(programador1);
-	appStore1.adicionarProgramador(programador2);
-	appStore1.adicionarProgramador(programador3);
-
 	
-	//Instanciar Aplicações e acrescentar às listas da AppStore e do programador
-	Aplicacao aplicacao1 = new Aplicacao("A1", 5, programador1, TipoAplicacao.GAMES);
-	Aplicacao aplicacao2 = new Aplicacao("A2", 10, programador1, TipoAplicacao.BUSINESS);
-	Aplicacao aplicacao3 = new Aplicacao("A3", 15, programador1, TipoAplicacao.GAMES);
-	Aplicacao aplicacao4 = new Aplicacao("A4", 20, programador1, TipoAplicacao.EDUCATION);
-	Aplicacao aplicacao5 = new Aplicacao("B1", 7, programador2, TipoAplicacao.GAMES);
-	Aplicacao aplicacao6 = new Aplicacao("B2", 17, programador2, TipoAplicacao.EDUCATION);
-	Aplicacao aplicacao7 = new Aplicacao("B3", 27, programador2, TipoAplicacao.GAMES);
-	Aplicacao aplicacao8 = new Aplicacao("C1", 4, programador3, TipoAplicacao.GAMES);
-	Aplicacao aplicacao9 = new Aplicacao("C2", 8, programador3, TipoAplicacao.GAMES);
-	Aplicacao aplicacao10 = new Aplicacao("C3", 12, programador3, TipoAplicacao.TRAVEL);
-	Aplicacao aplicacao11 = new Aplicacao("C4", 16, programador3, TipoAplicacao.TRAVEL);
-	Aplicacao aplicacao12 = new Aplicacao("C5", 20, programador3, TipoAplicacao.EDUCATION);
-
-	appStore1.adicionarAplicacao(aplicacao1);
-	appStore1.adicionarAplicacao(aplicacao2);
-	appStore1.adicionarAplicacao(aplicacao3);
-	appStore1.adicionarAplicacao(aplicacao4);
-	appStore1.adicionarAplicacao(aplicacao5);
-	appStore1.adicionarAplicacao(aplicacao6);
-	appStore1.adicionarAplicacao(aplicacao7);
-	appStore1.adicionarAplicacao(aplicacao8);
-	appStore1.adicionarAplicacao(aplicacao9);
-	appStore1.adicionarAplicacao(aplicacao10);
-	appStore1.adicionarAplicacao(aplicacao11);
-	appStore1.adicionarAplicacao(aplicacao12);
-	
-	//Instanciar Clientes e acrescentar à lista da AppStore
-	Cliente cliente1 = new Cliente("D",11);
-	Cliente cliente2 = new Cliente("E",21);
-	Cliente cliente3 = new Cliente("F",31);
-	
-	appStore1.adicionarCliente(cliente1);
-	appStore1.adicionarCliente(cliente2);
-	appStore1.adicionarCliente(cliente3);
 	
 	//Instanciar 1º cesto de compras
 	Cesto cesto1 = new Cesto();
